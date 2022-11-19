24019ad8ffd5455c640ae4b59b36289afb97992e
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-19 11:40:26 +0530

    Name Command Line

---
+++ Day1-FundamentalsPrograms/src/main/java/com/bridgelabz/Day1_FundamentalsPrograms/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Day1_FundamentalsPrograms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
