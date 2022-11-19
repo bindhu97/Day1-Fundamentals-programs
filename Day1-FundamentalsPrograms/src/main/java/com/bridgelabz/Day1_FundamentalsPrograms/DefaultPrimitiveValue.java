7c525407fd4848c5a7637bf621bafb65fc0eff3f
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-19 14:03:22 +0530

    DefaultPrimitiveValue

---
+++ Day1-FundamentalsPrograms/src/main/java/com/bridgelabz/Day1_FundamentalsPrograms/DefaultPrimitiveValue.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.Day1_FundamentalsPrograms;
+
+public class DefaultPrimitiveValue {
+
+	static byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bu;
+    
+    public static void main(String[] args) {
+    	DefaultPrimitiveValue def = new DefaultPrimitiveValue();
+        System.out.println(b);
+        System.out.println(def.s);
+        System.out.println(def.i);
+        System.out.println(def.l);
+        System.out.println(def.f);
+        System.out.println(def.d);
+        System.out.println(def.c);
+        System.out.println(def.bu);
+    }
+}
