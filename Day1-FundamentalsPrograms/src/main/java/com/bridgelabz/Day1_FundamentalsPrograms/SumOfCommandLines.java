c4d4f381f973f8abcc82f26aeb4791b6c7350283
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-19 13:46:46 +0530

    Sum Of Command Lines

---
+++ Day1-FundamentalsPrograms/src/main/java/com/bridgelabz/Day1_FundamentalsPrograms/SumOfCommandLines.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Day1_FundamentalsPrograms;
+
+import java.util.Scanner;
+
+public class SumOfCommandLines {
+
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+	        System.out.println("Enter any Number: ");
+	        int s = input.nextInt();
+	        int r = input.nextInt();
+	        int sum = s + r;
+	        System.out.println("Sum is " + sum);
+	        input.close();
+	}
+
+}
